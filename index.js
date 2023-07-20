4b7de82662548cabb74fea38cc29f97e013d51e7
pjpallickal <ppallickal@deloitte.com>
2023-07-20 11:17:27 +0530

    Changed message to notify the world.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
-  response.send('Hello Abdur Rehamn, Paul here!')
+  response.send('Hello world, Paul here!')
 })
 
 app.listen(app.get('port'), function () {
